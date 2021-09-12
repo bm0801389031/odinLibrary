bd23345e649c605f5b8660be52f1d5b278a3644f
Brian Monge <bm0801389031@gmail.com>
2021-09-12 15:58:19 -0700

    changed constructor function to class

--- logic.js
+++ logic.js
@@ -19,12 +19,18 @@ addBtn.addEventListener('click', addBookToLibrary);
 
 
 
-function Book(title, author, pages, read){
-    this.title = form.title.value; 
-    this.author = form.author.value; 
-    this.pages = form.pages.value + 'pg'; 
-    this.read = form.read.checked;
+
+
+class Book {
+    constructor(title, author, pages, read){
+        this.title = form.title.value;
+        this.author = form.author.value;
+        this.pages = form.pages.value + 'pg';
+        this.read = form.read.checked;
+    }
 }
+
+
 
 let myLibrary = []
 let newBook
